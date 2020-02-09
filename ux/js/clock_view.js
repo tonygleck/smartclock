e922c1217057e54a54ce83ce3e1e01cb4c39aa61
Jelani Brandon <jelanibrandon@gmail.com>
2020-02-09 08:05:27 -0800

    Updated the UI to show clock cleaner

--- ux/js/clock_view.js
+++ ux/js/clock_view.js
@@ -1,5 +1,28 @@
 var flash_time = false;
 
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var month = new Array(12);
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+
 function clock_type()
 {
     this.digital = 0;
@@ -15,6 +38,12 @@ function format_time(digits)
     return digits;
 }
 
+function format_date_value(curr_time)
+{
+    var format_date = month[curr_time.getMonth()] + " " +curr_time.getDay() + " " + curr_time.getFullYear() + " " + weekday[curr_time.getDay()];
+    return format_date;
+}
+
 function construct_clock()
 {
     var curr_time = new Date();
@@ -25,13 +54,19 @@ function construct_clock()
 
     //if (clock_face.face_type == this.digital)
     //{
-        var period = " a.m.";
+        var period = "AM";
         if (hour > 12)
         {
             hour = hour-12;
-            period = " p.m."
+            period = "PM"
         }
         var clock = document.getElementById('clock');
         clock.innerText = hour + ":" + min + ":" + sec;
-    //}
+
+        var period_obj = document.getElementById('period');
+        period_obj.innerText = period;
+
+        var clock_date = document.getElementById('clock_date');
+        clock_date.innerText = format_date_value(curr_time);
+        //}
 }
